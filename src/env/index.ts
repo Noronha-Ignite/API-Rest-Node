99acf6656c53a03344283fef3a1b6130af8c18ce
Gabriel Noronha <inc.691@gmail.com>
2023-09-30 23:04:23 -0300

    Set test environment sqlite database

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,18 +1,48 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
-const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  PORT: z.preprocess(
-    (a) => (a ? parseInt(z.string().max(5).parse(a), 10) : undefined),
-    z.number().positive().default(3333),
-  ),
-  DB_CLIENT: z.string(),
-  DB_HOST: z.string(),
-  DB_USER: z.string(),
-  DB_PASSWORD: z.string(),
-  DB_NAME: z.string(),
-})
+if (process.env.NODE_ENV === 'test') {
+  config({
+    path: '.env.test',
+  })
+} else {
+  config()
+}
+
+const envSchema = z
+  .object({
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('production'),
+    PORT: z.preprocess(
+      (a) => (a ? parseInt(z.string().max(5).parse(a), 10) : undefined),
+      z.number().positive().default(3333),
+    ),
+    DB_CLIENT: z.string(),
+
+    DB_HOST: z.string().optional(),
+    DB_USER: z.string().optional(),
+    DB_PASSWORD: z.string().optional(),
+    DB_NAME: z.string().optional(),
+
+    DB_URL: z.string().optional(),
+  })
+  .refine((data) => {
+    // If not test environment require connection fields
+    if (
+      data.NODE_ENV !== 'test' &&
+      !(data.DB_HOST && data.DB_NAME && data.DB_PASSWORD && data.DB_USER)
+    ) {
+      return false
+    }
+
+    // If test environment require db url field
+    if (data.NODE_ENV === 'test' && !data.DB_URL) {
+      return false
+    }
+
+    return true
+  })
 
 const _env = envSchema.safeParse(process.env)
 
--- src/libs/knexClient.ts
+++ src/libs/knexClient.ts
@@ -1,19 +1,34 @@
 import knex, { Knex } from 'knex'
 import { env } from '../env'
 
+let entensions: Knex.Config = {}
+
+if (env.NODE_ENV === 'test') {
+  entensions = {
+    connection: {
+      filename: env.DB_URL ?? '',
+    },
+    useNullAsDefault: true,
+  }
+} else {
+  entensions = {
+    connection: {
+      host: env.DB_HOST,
+      user: env.DB_USER,
+      password: env.DB_PASSWORD,
+      database: env.DB_NAME,
+    },
+  }
+}
+
 export const knexConfig: Knex.Config = {
   client: env.DB_CLIENT,
-  connection: {
-    host: env.DB_HOST,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
-  },
   migrations: {
     tableName: 'knex_migrations',
     directory: './src/database/migrations',
     extension: 'ts',
   },
+  ...entensions,
 }
 
 export const knexClient = knex(knexConfig)
